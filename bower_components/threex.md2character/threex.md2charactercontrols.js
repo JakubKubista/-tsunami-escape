6241c87c554b9a35fcd859677bbb2d25548ec9db
Jakub Kubišta <xkubista@gmail.com>
2017-12-05 18:54:52 +0100

    keys left/right, rotations left/right

--- bower_components/threex.md2character/threex.md2charactercontrols.js
+++ bower_components/threex.md2character/threex.md2charactercontrols.js
@@ -15,6 +15,8 @@ THREEx.MD2CharacterControls	= function(object3d, inputs){
 		left	: false,
 		up	: false,
 		down	: false,
+		turnRight	: false,
+		turnLeft	: false,
 		jump	: false,
 	}
 	inputs		= this.inputs
@@ -24,19 +26,19 @@ THREEx.MD2CharacterControls	= function(object3d, inputs){
 	this.linearSpeed	= 5.5		// default 2.5, superspeed 200
 
 	onRenderFcts.push(function(delta, now){
-		if( inputs.right )	object3d.rotation.y	-= this.angularSpeed*delta
-		if( inputs.left )	object3d.rotation.y	+= this.angularSpeed*delta
+		if( inputs.turnRight )	object3d.rotation.y	-= this.angularSpeed*delta
+		if( inputs.turnLeft )	object3d.rotation.y	+= this.angularSpeed*delta
 
 		// jump
 		var distance	= 0;
 		if( inputs.jump ){
 			if(object3d.position.y < .5){
-			distance	= +this.linearSpeed/2 * delta;
-			var velocity	= new THREE.Vector3(0, distance, 0);
-			object3d.position.add(velocity);
-		}else{
-			inputs.jump = false
-		}
+				distance	= +this.linearSpeed/2 * delta;
+				var velocity	= new THREE.Vector3(0, distance, 0);
+				object3d.position.add(velocity);
+			}else{
+				inputs.jump = false
+			}
 		}else{
 			if(object3d.position.y != 0){
 				console.log(object3d.position.y);
@@ -49,6 +51,11 @@ THREEx.MD2CharacterControls	= function(object3d, inputs){
 		// up/down
 		var distance	= 0;
 		if( inputs.up )		distance	= +this.linearSpeed * delta;
+		if( inputs.left )	{
+			object3d.rotation.y = 90
+			distance	= -this.linearSpeed * delta
+		}
+		if( inputs.right )		distance	= +this.linearSpeed * delta;
 		if( inputs.down )	distance	= -this.linearSpeed * delta;
 		if( distance ){
 			var velocity	= new THREE.Vector3(0, 0, distance);
