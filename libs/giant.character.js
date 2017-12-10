666a272e73d5b869664611d35d642a713cd436b3
Jakub Kubišta <xkubista@gmail.com>
2017-12-10 01:56:08 +0100

    add giant aditional animations

--- libs/giant.character.js
+++ libs/giant.character.js
@@ -6,11 +6,11 @@ function defaultValuesGiantCharacter(giantCharacter){
 	giantCharacter.character.scale = 1/5
 	giantCharacter.character.object3d.position.x = 0
 	giantCharacter.character.object3d.position.y = 4.5
-	giantCharacter.character.object3d.position.z = -30
+	giantCharacter.character.object3d.position.z = -40
 
 	giantCharacter.character.addEventListener('loaded', function(){
 		giantCharacter.setSkinName('dead')
 		giantCharacter.setWeaponName('none')
-		giantCharacter.setAnimationName('run')
+		giantCharacter.setAnimationName('flip')
 	})
 }
